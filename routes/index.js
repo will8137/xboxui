90485e800720d08e65c174122bb2a5a841afc3d8
Will Lytton <will.lytton@gmail.com>
2019-11-03 23:40:32 +0000

    add inital changes to tings

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var page = {
+	index: {
+		title: 'JDMS'
+	},
+	
+}
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', page);
 });
 
 module.exports = router;

8e2ad4edfc2f469a972b0437e75fa32ce19d82a2
Will Lytton <will.lytton@gmail.com>
2019-11-04 00:16:52 +0000

    Add potential protoyping to fs-extra

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var fs = require("fs-extra");
+
 var page = {
 	nav: [
 		{
@@ -12,14 +14,24 @@ var page = {
 			title: 'music'
 		},
 		{
-			title: 'video'
+			title: 'video',
+			content: {
+				folders: function getFolders() {
+					try {
+					  fs.copySync('/meida/', '/tmp/mynewfile')
+					  	console.log('success!')
+					} 
+					catch (err) {
+					  console.error(err)
+					}
+				};
+			}
 		},
 		{
 			title: 'sign in'
 		}
 	]
 }
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', page);
